d01ca4f07c19479c620b902fae1c3446eb583711
Michael Chandler <macjr93@yahoo.com>
2023-12-22 19:01:11 -0800

    add first suit in long method

--- index.js
+++ index.js
@@ -10,3 +10,33 @@ let drawFromDeckTop = () => {
     console.log(deck);
 }
 
+// add hearts suit (long method)
+console.log('card added to bottom of deck');
+addToDeckBottom('ace of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('two of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('three of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('four of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('five of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('six of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('seven of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('eight of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('nine of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('ten of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('jack of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('queen of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('king of hearts');
+console.log('card added to bottom of deck');
+addToDeckBottom('joker of hearts');
+
