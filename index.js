7c13e5e5ffa07add14f0b2cc6301ce9629a6db60
Michael Chandler <macjr93@yahoo.com>
2023-12-30 23:19:35 -0800

    prepare code for deck choosing mechanic

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const diamonds = [
-    'ace of diamonds', 'two of diamonds', 'three of diamonds', 'four of diamonds', 'five of diamonds', 'six of diamonds', 'seven of diamonds', 'eight of diamonds', 'nine of diamonds', 'ten of diamonds', 'jack of diamonds', 'queen of diamonds', 'king of diamonds', 'joker of diamonds'
+    'ace of diamonds', 'two of diamonds', 'three of diamonds', 'four of diamonds', 'five of diamonds', 'six of diamonds', 'seven of diamonds', 'eight of diamonds', 'nine of diamonds', 'ten of diamonds', 'jack of diamonds', 'queen of diamonds', 'king of diamonds'
 ]
 
 const hearts = [
-    'ace of hearts', 'two of hearts', 'three of hearts', 'four of hearts', 'five of hearts', 'six of hearts', 'seven of hearts', 'eight of hearts', 'nine of hearts', 'ten of hearts', 'jack of hearts', 'queen of hearts', 'king of hearts', 'joker of hearts'
+    'ace of hearts', 'two of hearts', 'three of hearts', 'four of hearts', 'five of hearts', 'six of hearts', 'seven of hearts', 'eight of hearts', 'nine of hearts', 'ten of hearts', 'jack of hearts', 'queen of hearts', 'king of hearts'
 ]
 
 const spades = [
-    'ace of spades', 'two of spades', 'three of spades', 'four of spades', 'five of spades', 'six of spades', 'seven of spades', 'eight of spades', 'nine of spades', 'ten of spades', 'jack of spades', 'queen of spades', 'king of spades', 'joker of spades'
+    'ace of spades', 'two of spades', 'three of spades', 'four of spades', 'five of spades', 'six of spades', 'seven of spades', 'eight of spades', 'nine of spades', 'ten of spades', 'jack of spades', 'queen of spades', 'king of spades'
 ]
 
 const clubs = [
-    'ace of clubs', 'two of clubs', 'three of clubs', 'four of clubs', 'five of clubs', 'six of clubs', 'seven of clubs', 'eight of clubs', 'nine of clubs', 'ten of clubs', 'jack of clubs', 'queen of clubs', 'king of clubs', 'joker of clubs'
+    'ace of clubs', 'two of clubs', 'three of clubs', 'four of clubs', 'five of clubs', 'six of clubs', 'seven of clubs', 'eight of clubs', 'nine of clubs', 'ten of clubs', 'jack of clubs', 'queen of clubs', 'king of clubs'
 ]
 
-const deck = {
-    cards: []
-};
+const jokers = [
+    'joker of clubs', 'joker of spades', 'joker of hearts', 'joker of diamonds'
+]
+
+const fiftyTwoCardDeck = [
+    diamonds + hearts + spades + clubs
+]
+
+const fullDeck = [
+    fiftyTwoCardDeck + jokers
+]
 
 const hand = {
     cards: []
@@ -37,5 +45,5 @@ let drawFromDeckTop = () => {
 console.log(deck);
 
 // draw a card
-console.log('you drew a card');
-drawFromDeckTop();
+// console.log('you drew a card');
+// drawFromDeckTop();
