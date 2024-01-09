fc32bb253f15af31599ef3f916bb981c5f006f01
Michael Chandler <macjr93@yahoo.com>
2024-01-08 16:39:53 -0800

    update deck arrays to object arrays

--- index.js
+++ index.js
@@ -1,88 +1,391 @@
-// data entry for suites
-const diamonds = [
-    'ace of diamonds', 'two of diamonds', 'three of diamonds', 'four of diamonds', 'five of diamonds', 'six of diamonds', 'seven of diamonds', 'eight of diamonds', 'nine of diamonds', 'ten of diamonds', 'jack of diamonds', 'queen of diamonds', 'king of diamonds'
-];
+// create playing decks
+const fullDeck = [
+    {
+        name: 'ace of diamonds',
+        suit: 'diamonds',
+        face: 'ace',
+        value: 1
+    },
+    {
+        name: 'two of diamonds',
+        suit: 'diamonds',
+        face: 'two',
+        value: 2
+    },
+    {
+        name: 'three of diamonds',
+        suit: 'diamonds',
+        face: 'three',
+        value: 3
+    },
+    {
+        name: 'four of diamonds',
+        suit: 'diamonds',
+        face: 'four',
+        value: 4
+    },
+    {
+        name: 'five of diamonds',
+        suit: 'diamonds',
+        face: 'five',
+        value: 5
+    },
+    {
+        name: 'six of diamonds',
+        suit: 'diamonds',
+        face: 'six',
+        value: 6
+    },
+    {
+        name: 'seven of diamonds',
+        suit: 'diamonds',
+        face: 'seven',
+        value: 7
+    },
+    {
+        name: 'eight of diamonds',
+        suit: 'diamonds',
+        face: 'eight',
+        value: 8
+    },
+    {
+        name: 'nine of diamonds',
+        suit: 'diamonds',
+        face: 'nine',
+        value: 9
+    },
+    {
+        name: 'ten of diamonds',
+        suit: 'diamonds',
+        face: 'ten',
+        value: 10
+    },
+    {
+        name: 'jack of diamonds',
+        suit: 'diamonds',
+        face: 'jack',
+        value: 11
+    },
+    {
+        name: 'queen of diamonds',
+        suit: 'diamonds',
+        face: 'queen',
+        value: 12
+    },
+    {
+        name: 'king of diamonds',
+        suit: 'diamonds',
+        face: 'king',
+        value: 13
+    },
+    {
+        name: 'joker of diamonds',
+        suit: 'diamonds',
+        face: 'joker',
+        value: 0
+    },
+    {
+        name: 'ace of hearts',
+        suit: 'hearts',
+        face: 'ace',
+        value: 1
+    },
+    {
+        name: 'two of hearts',
+        suit: 'hearts',
+        face: 'two',
+        value: 2
+    },
+    {
+        name: 'three of hearts',
+        suit: 'hearts',
+        face: 'three',
+        value: 3
+    },
+    {
+        name: 'four of hearts',
+        suit: 'hearts',
+        face: 'four',
+        value: 4
+    },
+    {
+        name: 'five of hearts',
+        suit: 'hearts',
+        face: 'five',
+        value: 5
+    },
+    {
+        name: 'six of hearts',
+        suit: 'hearts',
+        face: 'six',
+        value: 6
+    },
+    {
+        name: 'seven of hearts',
+        suit: 'hearts',
+        face: 'seven',
+        value: 7
+    },
+    {
+        name: 'eight of hearts',
+        suit: 'hearts',
+        face: 'eight',
+        value: 8
+    },
+    {
+        name: 'nine of hearts',
+        suit: 'hearts',
+        face: 'nine',
+        value: 9
+    },
+    {
+        name: 'ten of hearts',
+        suit: 'hearts',
+        face: 'ten',
+        value: 10
+    },
+    {
+        name: 'jack of hearts',
+        suit: 'hearts',
+        face: 'jack',
+        value: 11
+    },
+    {
+        name: 'queen of hearts',
+        suit: 'hearts',
+        face: 'queen',
+        value: 12
+    },
+    {
+        name: 'king of hearts',
+        suit: 'hearts',
+        face: 'king',
+        value: 13
+    },
+    {
+        name: 'joker of hearts',
+        suit: 'hearts',
+        face: 'joker',
+        value: 0
+    },
+    {
+        name: 'ace of spades',
+        suit: 'spades',
+        face: 'ace',
+        value: 1
+    },
+    {
+        name: 'two of spades',
+        suit: 'spades',
+        face: 'two',
+        value: 2
+    },
+    {
+        name: 'three of spades',
+        suit: 'spades',
+        face: 'three',
+        value: 3
+    },
+    {
+        name: 'four of spades',
+        suit: 'spades',
+        face: 'four',
+        value: 4
+    },
+    {
+        name: 'five of spades',
+        suit: 'spades',
+        face: 'five',
+        value: 5
+    },
+    {
+        name: 'six of spades',
+        suit: 'spades',
+        face: 'six',
+        value: 6
+    },
+    {
+        name: 'seven of spades',
+        suit: 'spades',
+        face: 'seven',
+        value: 7
+    },
+    {
+        name: 'eight of spades',
+        suit: 'spades',
+        face: 'eight',
+        value: 8
+    },
+    {
+        name: 'nine of spades',
+        suit: 'spades',
+        face: 'nine',
+        value: 9
+    },
+    {
+        name: 'ten of spades',
+        suit: 'spades',
+        face: 'ten',
+        value: 10
+    },
+    {
+        name: 'jack of spades',
+        suit: 'spades',
+        face: 'jack',
+        value: 11
+    },
+    {
+        name: 'queen of spades',
+        suit: 'spades',
+        face: 'queen',
+        value: 12
+    },
+    {
+        name: 'king of spades',
+        suit: 'spades',
+        face: 'king',
+        value: 13
+    },
+    {
+        name: 'joker of spades',
+        suit: 'spades',
+        face: 'joker',
+        value: 0
+    },
+    {
+        name: 'ace of clubs',
+        suit: 'clubs',
+        face: 'ace',
+        value: 1
+    },
+    {
+        name: 'two of clubs',
+        suit: 'clubs',
+        face: 'two',
+        value: 2
+    },
+    {
+        name: 'three of clubs',
+        suit: 'clubs',
+        face: 'three',
+        value: 3
+    },
+    {
+        name: 'four of clubs',
+        suit: 'clubs',
+        face: 'four',
+        value: 4
+    },
+    {
+        name: 'five of clubs',
+        suit: 'clubs',
+        face: 'five',
+        value: 5
+    },
+    {
+        name: 'six of clubs',
+        suit: 'clubs',
+        face: 'six',
+        value: 6
+    },
+    {
+        name: 'seven of clubs',
+        suit: 'clubs',
+        face: 'seven',
+        value: 7
+    },
+    {
+        name: 'eight of clubs',
+        suit: 'clubs',
+        face: 'eight',
+        value: 8
+    },
+    {
+        name: 'nine of clubs',
+        suit: 'clubs',
+        face: 'nine',
+        value: 9
+    },
+    {
+        name: 'ten of clubs',
+        suit: 'clubs',
+        face: 'ten',
+        value: 10
+    },
+    {
+        name: 'jack of clubs',
+        suit: 'clubs',
+        face: 'jack',
+        value: 11
+    },
+    {
+        name: 'queen of clubs',
+        suit: 'clubs',
+        face: 'queen',
+        value: 12
+    },
+    {
+        name: 'king of clubs',
+        suit: 'clubs',
+        face: 'king',
+        value: 13
+    },
+    {
+        name: 'joker of clubs',
+        suit: 'clubs',
+        face: 'joker',
+        value: 0
+    },
+]
 
-const hearts = [
-    'ace of hearts', 'two of hearts', 'three of hearts', 'four of hearts', 'five of hearts', 'six of hearts', 'seven of hearts', 'eight of hearts', 'nine of hearts', 'ten of hearts', 'jack of hearts', 'queen of hearts', 'king of hearts'
-];
-
-const spades = [
-    'ace of spades', 'two of spades', 'three of spades', 'four of spades', 'five of spades', 'six of spades', 'seven of spades', 'eight of spades', 'nine of spades', 'ten of spades', 'jack of spades', 'queen of spades', 'king of spades'
-];
-
-const clubs = [
-    'ace of clubs', 'two of clubs', 'three of clubs', 'four of clubs', 'five of clubs', 'six of clubs', 'seven of clubs', 'eight of clubs', 'nine of clubs', 'ten of clubs', 'jack of clubs', 'queen of clubs', 'king of clubs'
-];
-
-const jokers = [
-    'joker of clubs', 'joker of spades', 'joker of hearts', 'joker of diamonds'
-];
-
-// create arrays for decks used behind the scenes
-const fiftyTwoCardDeck = [];
-
-const fullDeck = [];
-
-// create objects for decks used by user
-const playingDeck = {
-    cards: []
-};
-
-const hand = {
-    cards: []
-};
+const fiftyTwo = fullDeck.filter(card => card.face !== 'joker')
 
 // setup basic functions
-let barker = (input) => {
-    console.log(input);
-    confirm(input);
+const barker = (deck) => {
+    console.log(deck.name);
+    confirm(deck.name);
 }
 
-let createDeck = (donorDeck, recieveDeck) => {
-    for (let cardID = 0; cardID < donorDeck.length; cardID++) {
-        // console.log(donorDeck[cardID])
-        recieveDeck.push(donorDeck[cardID]);
-    }
-    // barker(`here is your deck: ${recieveDeck}`);
-}
 
-let drawFromDeckTop = (num) => {
-    for (let repeat = 0; repeat <= parseInt(num); num++) {
-        let card = playingDeck.cards.shift();
-        // console.log('This is your deck: ' + deck.cards);
-        hand.cards.unshift(card);
-        // console.log('This is your hand: ' + hand.cards);
-    }
-    barker(`here is your hand: ${hand.cards}`);
-}
+barker(fiftyTwo);
+barker(fullDeck);
 
-// let addToDeckBottom = (obj) => {
+// const drawFromDeckTop = (num) => {
+//     for (let repeat = 0; repeat <= parseInt(num); num++) {
+//         let card = playingDeck.cards.shift();
+//         // console.log('This is your deck: ' + deck.cards);
+//         hand.cards.unshift(card);
+//         // console.log('This is your hand: ' + hand.cards);
+//     }
+//     barker(`here is your hand: ${hand.cards}`);
+// }
+
+// const addToDeckBottom = (obj) => {
 //     deck.cards.push(obj);
 //     // console.log('This is your deck: ' + deck.cards);
 // }
 
-// create background decks
-createDeck(diamonds, fiftyTwoCardDeck);
-createDeck(hearts, fiftyTwoCardDeck);
-createDeck(spades, fiftyTwoCardDeck);
-createDeck(clubs, fiftyTwoCardDeck);
-// barker(`here is your deck: ${fiftyTwoCardDeck}`);
-
-createDeck(fiftyTwoCardDeck, fullDeck);
-createDeck(jokers, fullDeck);
-// barker(`here is your deck: ${fullDeck}`);
-
 // create playing deck
-let input = prompt("Between the fullDeck and fiftyTwoCardDeck which would you like to use");
-console.log(input);
-if (input === fullDeck / input === fiftyTwoCardDeck) {
-    createDeck(input, playingDeck.cards);
-    barker(`here is your deck: ${playingDeck.cards}`);
-} else {
-    createDeck(fullDeck, playingDeck.cards);
-    barker(`here is your deck: ${playingDeck.cards}`);
-}
+
+// let input = prompt("Between the fullDeck and fiftyTwo which would you like to use?");
+// console.log(input);
+// if (input === 'fullDeck') {
+//     const playingDeck = fullDeck.map(function (card) {
+//         return card;
+//     });
+//     barker(`here is your deck: ${playingDeck.cards}`);
+// } else if (input === 'fiftyTwo') {
+//     const playingDeck = fiftyTwo.map(function (card) {
+//         return card;
+//     });
+//     barker(`here is your deck: ${playingDeck.cards}`);
+// } else {
+//     const playingDeck = fullDeck.map(function (card) {
+//         return card;
+//     });
+//     barker(`here is your deck: ${playingDeck.cards}`);
+// }
+
 // draw a card
 // drawFromDeckTop(1);
 
